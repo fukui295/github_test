b0793e8d5ae477039d3427b3d14f625f56c2306a
fukui295 <136450187+fukui295@users.noreply.github.com>
2023-08-03 17:33:07 +0900

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -9,4 +9,7 @@ input.onGesture(Gesture.TiltLeft, function () {
 })
 basic.forever(function () {
     basic.showIcon(IconNames.Heart)
+    basic.pause(100)
+    basic.clearScreen()
+    basic.pause(100)
 })
